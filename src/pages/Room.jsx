d670b7b1272d334e7736ee755131a1239878fc6d
Anand Raj <anandraj.solutions@gmail.com>
2023-12-17 01:01:29 +0530

    started basic working

--- src/pages/Room.jsx
+++ src/pages/Room.jsx
@@ -1,56 +1,75 @@
 import React, { useEffect, useState, useRef } from 'react'
 import { databases } from '../appwriteConfig'
+import { ID } from 'appwrite'
 
 import "./style.scss"
 const Room = () => {
 
-  //form
-    
-  const [inputValue, setInputValue] = useState('');
-    
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // Do something with the form data
-    console.log('Form submitted with value:', inputValue);
-  };
+  
 
+  //form
+  const [inputValue, setInputValue] = useState('');
   const handleInputChange = (e) => {
     setInputValue(e.target.value);
   };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-
-
-  const [messages, setMessages] = useState(null);
-  
-    const getMessages = async () => {
-      const response = await databases.listDocuments(import.meta.env.VITE_DATABASE_ID,import.meta.env.VITE_COLLECTION_ID);
-      const filteredResponse = response.documents.map(doc => {
-        const { $collectionId,$databaseId,$id,...rest } = doc;
-        return rest;
-    });
-      
-      setMessages(filteredResponse)
+    let payload = {
+      body:inputValue
     }
-    
-    useEffect(()=>{
-      getMessages()
-    },[])
-    //rightMsg or leftMsg
 
-    //scrool to bottom
-    const messagesEndRef = useRef(null);
-    const scrollToBottom = () => {
-      if (messagesEndRef.current) {
-        messagesEndRef.current.scrollTop = messagesEndRef.current.scrollHeight;
-      }
-    };
+    const response =await databases.createDocument(
+      import.meta.env.VITE_DATABASE_ID,
+      import.meta.env.VITE_COLLECTION_ID,
+      ID.unique(),
+      payload
+    );
+
+    console.log('Form submitted', response);
+    setMessages(prevState => [...messages,response])
+    setInputValue('')
+  };
   
-    useEffect(() => {
-      scrollToBottom();
-    }, []);
+
+
+  //addMessages
+
+
+  //listMessages
+  const [messages, setMessages] = useState([]);
+  const getMessages = async () => {
+    const response = await databases.listDocuments(import.meta.env.VITE_DATABASE_ID,import.meta.env.VITE_COLLECTION_ID);
+    const filteredResponse = response.documents.map(doc => {
+      const { $collectionId,$databaseId,...rest } = doc;
+      return rest;
+    });
+    setMessages(filteredResponse)
+  }
+  useEffect(()=>{
+    getMessages()
+  },[])
+    
+
+  //scrool to bottom
+  const messagesEndRef = useRef(null);
+  const scrollToBottom = () => {
+    if (messagesEndRef.current) {
+      messagesEndRef.current.scrollTop = messagesEndRef.current.scrollHeight;
+    }
+  };
+
+  useEffect(() => {
+    scrollToBottom();
+  }, []);
+    
   
     
-    
+  //rightMsg or leftMsg
+    // <div className="messageItems leftMsg">
+    //   <div className="messageUser">anand</div>
+    //   <div className="messageContent bg-slate-700">Hello googler</div>
+    // </div>
 
   return (
     <div className='mainContainer' >
@@ -59,43 +78,12 @@ const Room = () => {
       </div>
       <div className="contentBody bg-slate-500 ">
         <div className="messages " ref={messagesEndRef}>
-              
-              <div className="messageItems rightMsg">
-                <div className="messageUser">anand</div>
-                <div className="messageContent bg-slate-700">Hello googler</div>
+              {messages.map((message)=>(
+                <div className="messageItems leftMsg" key={message.$id}>
+                <div className="messageUser">{message.username}</div>
+                <div className="messageContent bg-slate-700">{message.body}</div>
               </div>
-              <div className="messageItems leftMsg">
-                <div className="messageUser">anand</div>
-                <div className="messageContent bg-slate-700">Hello googler</div>
-              </div>
-              <div className="messageItems leftMsg">
-                <div className="messageUser">anand</div>
-                <div className="messageContent bg-slate-700">Hello googler</div>
-              </div>
-              <div className="messageItems leftMsg">
-                <div className="messageUser">anand</div>
-                <div className="messageContent bg-slate-700">Hello googler</div>
-              </div>
-              <div className="messageItems rightMsg">
-                <div className="messageUser">anand</div>
-                <div className="messageContent bg-slate-700">Hello googler</div>
-              </div>
-              <div className="messageItems leftMsg">
-                <div className="messageUser">anand</div>
-                <div className="messageContent bg-slate-700">Hello googler</div>
-              </div>
-              <div className="messageItems leftMsg">
-                <div className="messageUser">anand</div>
-                <div className="messageContent bg-slate-700">Hello googler</div>
-              </div>
-              <div className="messageItems leftMsg">
-                <div className="messageUser">anand</div>
-                <div className="messageContent bg-slate-700">Hello googler</div>
-              </div>
-              
-              
-              
-          
+              ))}
         </div>
         
         <form className="input-form" onSubmit={handleSubmit}>
