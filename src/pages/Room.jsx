42e2fd935c75f53f330b7bf1468acda2fdd75eeb
Anand Raj <anandraj.solutions@gmail.com>
2023-12-16 23:23:06 +0530

    completed the major designing in desktop view

--- src/pages/Room.jsx
+++ src/pages/Room.jsx
@@ -1,35 +1,56 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useRef } from 'react'
 import { databases } from '../appwriteConfig'
 
 import "./style.scss"
 const Room = () => {
 
+  //form
+    
+  const [inputValue, setInputValue] = useState('');
+    
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Do something with the form data
+    console.log('Form submitted with value:', inputValue);
+  };
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+
+
+  const [messages, setMessages] = useState(null);
+  
     const getMessages = async () => {
       const response = await databases.listDocuments(import.meta.env.VITE_DATABASE_ID,import.meta.env.VITE_COLLECTION_ID);
       const filteredResponse = response.documents.map(doc => {
         const { $collectionId,$databaseId,$id,...rest } = doc;
         return rest;
     });
-      console.log(filteredResponse)
+      
+      setMessages(filteredResponse)
     }
-
+    
     useEffect(()=>{
       getMessages()
     },[])
+    //rightMsg or leftMsg
 
-    //form
+    //scrool to bottom
+    const messagesEndRef = useRef(null);
+    const scrollToBottom = () => {
+      if (messagesEndRef.current) {
+        messagesEndRef.current.scrollTop = messagesEndRef.current.scrollHeight;
+      }
+    };
+  
+    useEffect(() => {
+      scrollToBottom();
+    }, []);
+  
     
-      const [inputValue, setInputValue] = useState('');
     
-      const handleSubmit = (e) => {
-        e.preventDefault();
-        // Do something with the form data
-        console.log('Form submitted with value:', inputValue);
-      };
-    
-      const handleInputChange = (e) => {
-        setInputValue(e.target.value);
-      };
 
   return (
     <div className='mainContainer' >
@@ -37,8 +58,44 @@ const Room = () => {
         <div className='bg-slate-700 logo'>Logo</div>
       </div>
       <div className="contentBody bg-slate-500 ">
-        <div className="messages bg-slate-700 ">
-          <div className="messageItems bg-slate-500">Message</div>
+        <div className="messages " ref={messagesEndRef}>
+              
+              <div className="messageItems rightMsg">
+                <div className="messageUser">anand</div>
+                <div className="messageContent bg-slate-700">Hello googler</div>
+              </div>
+              <div className="messageItems leftMsg">
+                <div className="messageUser">anand</div>
+                <div className="messageContent bg-slate-700">Hello googler</div>
+              </div>
+              <div className="messageItems leftMsg">
+                <div className="messageUser">anand</div>
+                <div className="messageContent bg-slate-700">Hello googler</div>
+              </div>
+              <div className="messageItems leftMsg">
+                <div className="messageUser">anand</div>
+                <div className="messageContent bg-slate-700">Hello googler</div>
+              </div>
+              <div className="messageItems rightMsg">
+                <div className="messageUser">anand</div>
+                <div className="messageContent bg-slate-700">Hello googler</div>
+              </div>
+              <div className="messageItems leftMsg">
+                <div className="messageUser">anand</div>
+                <div className="messageContent bg-slate-700">Hello googler</div>
+              </div>
+              <div className="messageItems leftMsg">
+                <div className="messageUser">anand</div>
+                <div className="messageContent bg-slate-700">Hello googler</div>
+              </div>
+              <div className="messageItems leftMsg">
+                <div className="messageUser">anand</div>
+                <div className="messageContent bg-slate-700">Hello googler</div>
+              </div>
+              
+              
+              
+          
         </div>
         
         <form className="input-form" onSubmit={handleSubmit}>
@@ -50,7 +107,7 @@ const Room = () => {
               placeholder="Enter text here"
               className="input-field"
             />
-            <button type="submit" className="submit-button">
+            <button type="submit"  className="submit-button">
              ↩
             </button>
           </div>
